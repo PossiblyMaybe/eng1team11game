62781655648378ad596257860553ee280d2a506a
Will Steven <william.steven51218@gmail.com>
2025-10-07 20:53:18 +0100

    condensed the opening logo screens into one because it was basically just copied code.

--- core/src/main/java/t11/game/Main.java
+++ core/src/main/java/t11/game/Main.java
@@ -3,51 +3,41 @@ package t11.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
-
-import t11.game.ScreenDispatch;
+import com.badlogic.gdx.graphics.g2d.SpriteCache;
 
 public class Main extends Game {
-	private SpriteBatch batch;
-	private Texture image;
-	private Screen test;
-	private Screen test3;
+	private SpriteCache cache;
+	private double timeElapsed;
+	
+	private Screen openingLogos;
 
-	private ScreenDispatch test2;
-	private static long timeStarted;
-	private static long timeElapsed;
+	private ScreenDispatch test;
+	
 	@Override
 	public void create() {
-        	batch = new SpriteBatch();
-		test = new LibGDXScreen(batch);
-		test3 = new Team11LogoScreen(batch);
-    		test2 = new ScreenDispatch();
-		test2.addScreen(test);
-		System.out.println("test2");
-		this.setScreen(test);
-		this.timeStarted = System.currentTimeMillis();
+        	cache = new SpriteCache();
+		
+		String[] pngs = {"libgdx.png","teamLogo.png"};
+		openingLogos = new LogoScreen(cache, pngs);
+
+		
+		test = new ScreenDispatch();
+		test.addScreen(openingLogos);
+
+		this.setScreen(openingLogos);
+		
 	}
 
     	@Override
     	public void render() {
 		super.render();
-		this.timeElapsed = System.currentTimeMillis() - this.timeStarted;
-		this.nextScreen();
 	}
 
     	@Override
     	public void dispose() {
-        	batch.dispose();
+        	cache.dispose();
     	}
 
-	private void nextScreen(){
-		if (this.timeElapsed > 10000){
-			return;
-		} else if (this.timeElapsed > 4000){
-			this.setScreen(test3);
-		}
-	}
 }
