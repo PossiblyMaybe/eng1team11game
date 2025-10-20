3d5cacd3a9c33fe4f35e43e3870edbc660e3284e
BGSHENTAN <qsv526@york.ac.uk>
2025-10-20 15:52:36 +0800

    Update EventCounter.java

--- core/src/main/java/t11/game/EventCounter.java
+++ core/src/main/java/t11/game/EventCounter.java
@@ -6,13 +6,22 @@ public class SimpleEventCounter {
     private int hidden   = 0;
 
     // increment by type
-    public void inc(EventType type) {
-        switch (type) {
-            case POSITIVE -> positive++;
-            case NEGATIVE -> negative++;
-            case HIDDEN   -> hidden++;
+   public void inc(EventType type) {
+    switch (type) {
+        case POSITIVE:
+            positive++;
+            break;
+        case NEGATIVE:
+            negative++;
+            break;
+        case HIDDEN:
+            hidden++;
+            break;
+        default:
+            break;
         }
     }
+
 
     // direct increments (optional helpers)
     public void incPositive() { positive++; }
